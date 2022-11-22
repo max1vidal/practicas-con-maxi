a9272959a131e7258c946ceec0c71fa57147efb7
max1vidal <112936988+max1vidal@users.noreply.github.com>
2022-11-22 11:14:51 -0300

    Update tarjeta.php

--- practicas/tarjeta.php
+++ practicas/tarjeta.php
@@ -10,4 +10,15 @@
 <p>dsghghdgashdgajhs</p>
 <a href="<?php echo $instagram;?>">Ingrese al instgram</a>
 <a href="<?php echo $nuevoInstagram;?>">Ingrese al instgram</a>
+
+<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="imagenes/Logo-removebg-preview.png" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">esc tec n°5</h5>
+      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut id fuga voluptate magnam hic
+        temporibus harum non iste ratione, minima modi exercitationem quidem consequatur placeat, ea culpa sequi veniam
+        neque.</p>
+      <a href="https://www.instagram.com/damiancamisay/" class="btn btn-primary">redes</a>
+    </div>
+  </div>
 </body>
